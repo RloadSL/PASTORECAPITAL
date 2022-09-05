f2648a3b82694fb3b4040b63d37f957da372f4ec
Maria <maria@rload.es>
2022-09-05 10:53:26 +0200

    drawerNav structure fixed

--- components/AppLayout/AppLayout.tsx
+++ components/AppLayout/AppLayout.tsx
@@ -1,17 +1,15 @@
 import Footer from './components/Footer'
 import DrawerNav from './components/DrawerNav'
-import styles from './AppLayout.module.scss'
+import style from './AppLayout.module.scss'
 import NavBar from './components/NavBar'
 import Form from 'components/Form/Form'
 
-export default function AppLayout ({ children }: any) {
+export default function AppLayout({ children }: any) {
   return (
-    <div className={styles.grid}>
-    <NavBar/>
+    <div>
       <DrawerNav>
         {children}
       </DrawerNav>
-      <Footer/>
     </div>
   )
 }
