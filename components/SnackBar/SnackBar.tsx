2d2e01468634b171f0b343c89dbd28f413ebe4ba
joseacemedina <joseacemedina@gmail.com>
2022-09-14 11:06:21 +0200

    fix snackbar

--- components/SnackBar/SnackBar.tsx
+++ components/SnackBar/SnackBar.tsx
@@ -5,8 +5,11 @@ import { FormattedMessage } from 'react-intl'
 import { useSystem } from 'ui/hooks/system.hooks'
 import style from './SnackBar.module.scss'
 
+
+export type SNACKBARTYPE  =  'warn' | 'error' | 'info' | 'success'
 interface SNACKBARPROPS {
-  time?: number
+  time?: number,
+  type: SNACKBARTYPE
 }
 
 /**
@@ -15,7 +18,7 @@ interface SNACKBARPROPS {
  * @returns 
  */
 
-export const SnackBar = ({ time = 5000 }: SNACKBARPROPS) => {
+export const SnackBar = ({ time = 5000, type = 'info' }: SNACKBARPROPS) => {
   const {errorsApp, cleanErrorsApp} = useSystem()
   useEffect(() => {
     if (errorsApp.length > 0) setTimeout(() => {
@@ -24,14 +27,14 @@ export const SnackBar = ({ time = 5000 }: SNACKBARPROPS) => {
   }, [errorsApp.length])
 
   return (
-    <SnackBarView errorsApp={errorsApp} />
+    <SnackBarView errorsApp={errorsApp} type={type}/>
   )
 }
 
-const SnackBarView = ({ errorsApp }: { errorsApp: ErrorApp[] }) => {
+const SnackBarView = ({ errorsApp, type }: { errorsApp: ErrorApp[], type: SNACKBARTYPE }) => {
   return (
     <div className={`${style.snackBar} ${errorsApp.length > 0 ? style.show : ''}`}>
-      {errorsApp.map((err, index:number) => <div key={index} className={style.container}>
+      {errorsApp.map((err, index:number) => <div key={index} className={`${style.container} ${style[type]}`}>
         <p ><FormattedMessage id={err.errorCode}></FormattedMessage></p>
       </div>)}
     </div>
