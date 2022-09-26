3b5ccecb368fd5ff8fae1ffead349a27c5fd2da1
joseacemedina <joseacemedina@gmail.com>
2022-09-26 16:14:32 +0200

    fix post excerpt

--- components/PostExcerpt/PostExcerpt.tsx
+++ components/PostExcerpt/PostExcerpt.tsx
@@ -1,46 +1,54 @@
-import Chips from 'components/Chips';
+import Chips from 'components/Chips'
 import Image from 'next/image'
-import { useComponentUtils } from 'ui/hooks/components.hooks';
-import style from './PostExcerpt.module.scss';
-import parse from 'html-react-parser';
-import React from 'react';
-
+import { useComponentUtils } from 'ui/hooks/components.hooks'
+import style from './PostExcerpt.module.scss'
+import parse from 'html-react-parser'
+import React from 'react'
+import { WpCat } from 'infrastructure/dto/course.dto'
 
 type THUMBNAIL = {
-  imgUrl: string,
+  imgUrl: string
   altText: string
 }
 
 export interface POSTEXCERPTPROPS {
-
-  title?: string,
-  description: string,
-  thumbnail?: string,
+  title?: string
+  description: string
+  thumbnail?: string
   terms?: Array<any>
-  level?: Array<any>
+  level?: WpCat
 }
 
 /**
  * Función principal del componente de extracto del post
  * @param gridItems Listado de elementos para renderizar en el grid
- * @returns 
+ * @returns
  */
 
-const PostExcerpt = ({ title, description, thumbnail, terms, level }: POSTEXCERPTPROPS) => {
-
+const PostExcerpt = ({
+  title,
+  description,
+  thumbnail,
+  terms,
+  level
+}: POSTEXCERPTPROPS) => {
   const { limitTextLength } = useComponentUtils()
-
   return (
     <div>
+      <div className={style.imageContainer}>
+        {level && <div className={style.level}>{level.slug}</div>}
+        {thumbnail && <Image layout='fill' src={thumbnail} alt={title} />}
+      </div>
 
-      {thumbnail ? <div className={style.imageContainer}><Image layout='fill' src={thumbnail} alt={title}/></div> : null}
-      <p className={style.title}>{limitTextLength(60, title || '')}</p>
+      <p className={style.title}>{limitTextLength(60, title || '')}</p>
       <div className={style.description}>{parse(description)}</div>
 
       <div className={style.terms}>
-        {terms ? <Chips chips={terms} color='lightMain' /> : null}
+        {terms ? (
+          <Chips chips={terms.map(item => item.slug)} color='lightMain' />
+        ) : null}
       </div>
     </div>
   )
 }
-export default  React.memo(PostExcerpt);
+export default React.memo(PostExcerpt)
--- domain/Course/Course.tsx
+++ domain/Course/Course.tsx
@@ -1,101 +1,110 @@
-import { CourseDto } from "infrastructure/dto/course.dto";
-import { WP_EDIT_POST } from "infrastructure/wordpress/config";
+import { CourseDto, WpCat, WpTerm } from 'infrastructure/dto/course.dto'
+import { WP_EDIT_POST } from 'infrastructure/wordpress/config'
 
+const levels = ['basic', 'advanced', 'medium']
 export class Course {
-  private _id: string;
-  public get id(): string {
-    return this._id;
+  private _id: string
+  public get id (): string {
+    return this._id
   }
 
-
-  private _wpID: number;
-  public get wpID(): number {
-    return this._wpID;
+  private _wpID: number
+  public get wpID (): number {
+    return this._wpID
   }
 
-  private _categories: Array<number>;
-  public get categories(): Array<number> {
-    return this._categories;
+  private _categories: WpCat[]
+  public get categories (): WpCat[] {
+    return this._categories
   }
-  
-  private _status: string;
-  public get status(): string {
-    return this._status;
+
+  public get level (): WpCat | undefined {
+    return this._categories.find(item => levels.includes(item.slug))
+  }
+
+  private _tags: WpTerm[]
+  public get tags (): WpTerm[] {
+    return this._tags
+  }
+
+  private _status: string
+  public get status (): string {
+    return this._status
   }
   private _excerpt: {
-    rendered: string,
+    rendered: string
     raw: string
   }
-  public get excerpt(): {
-    rendered: string,
+  public get excerpt (): {
+    rendered: string
     raw: string
   } {
-    return this._excerpt;
-  };
+    return this._excerpt
+  }
 
-  private _thumbnail_url: string;
-  public get  thumbnail_url(): string {
-    return this._thumbnail_url;
-  };
+  private _thumbnail_url: string
+  public get thumbnail_url (): string {
+    return this._thumbnail_url
+  }
 
-  private _slug: string;
-  public get slug(): string {
-    return this._slug;
-  };
-  
-
- 
+  private _slug: string
+  public get slug (): string {
+    return this._slug
+  }
 
   private _title: {
-    rendered: string,
+    rendered: string
     raw: string
-  };
-  public get title(): {
-    rendered: string,
+  }
+  public get title (): {
+    rendered: string
     raw: string
   } {
-    return this._title;
-  };
+    return this._title
+  }
 
   private _content?: {
-    rendered: string,
+    rendered: string
     raw: string
-  };
-  public get content(): {
-    rendered: string,
-    raw: string
-  } | undefined{
-    return this._content;
-  };
+  }
+  public get content ():{
+        rendered: string
+        raw: string
+      }
+    | undefined {
+    return this._content
+  }
   /**
    * ISO Date convertir a new Date
    */
   private _created_at: Date
-  public get created_at(): Date {
-    return this._created_at;
-  };
-  constructor(courseData:CourseDto) {
+  public get created_at (): Date {
+    return this._created_at
+  }
+  constructor (courseData: CourseDto) {
     this._id = courseData.id.toString()
+    this._tags = courseData.tags
     this._thumbnail_url = courseData.thumbnail_url
     this._wpID = courseData.id
     this._categories = courseData.categories
-    this._status= courseData.status
-    this._excerpt= courseData.excerpt,
-    this._content = courseData.content,
-    this._slug= courseData.slug,
+    this._status = courseData.status
+    this._excerpt = courseData.excerpt
+    this._content = courseData.content
+    this._slug = courseData.slug
     this._title = courseData.title
-    this._created_at =   new Date(courseData.date)
+    this._created_at = new Date(courseData.date)
   }
 
-  public toJson = ():CourseDto => ({
-    id : this._wpID ,
-    categories  : this._categories,
+  public toJson = (): CourseDto => ({
+    id: this._wpID,
+    categories: this._categories,
     status: this._status,
-    excerpt : this._excerpt,
+    excerpt: this._excerpt,
     slug: this._slug,
-    title : this._title,
-    date : this._created_at,
+    title: this._title,
+    date: this._created_at,
     content: this._content,
-    thumbnail_url: this._thumbnail_url
-  });
+    thumbnail_url: this._thumbnail_url,
+    tags: this._tags
+  })
 }
