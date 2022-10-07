23b639ea750c617a5745214cfc6b90205c1c9e7d
joseacemedina <joseacemedina@gmail.com>
2022-10-07 09:51:46 +0200

    Adding configurations files

---
+++ infrastructure/firebase/config.ts
@@ -0,0 +1,1 @@
+export const on_cloud_firebase = true;
---
+++ infrastructure/wordpress/config.ts
@@ -0,0 +1,12 @@
+import { on_cloud_firebase } from "infrastructure/firebase/config";
+
+const WP_HOST = !on_cloud_firebase ? 'http://localhost:8888/pastoreheadless/' : 'https://rload.es/pastoreheadless/';
+const WP_API = `${WP_HOST}wp-json/wp/v2/`;
+
+export const WP_API_CATEGORY = `${WP_API}categories`;
+export const WP_API_TAGS = `${WP_API}tags`;
+export const WP_API_PAGES = `${WP_API}pages`;
+export const WP_EDIT_POST = `${WP_HOST}wp-admin/post.php`; 
+export const WP_API_PAGES_LIST = `${WP_API}pages?per_page=9&orderby=date`;
+export const WP_API_POST = `${WP_API}posts`;
+
