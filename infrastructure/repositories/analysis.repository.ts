6b71e00f99349073664d89fe74292da11da9121e
joseacemedina <joseacemedina@gmail.com>
2022-10-27 11:42:54 +0200

    Analysis

--- infrastructure/repositories/analysis.repository.ts
+++ infrastructure/repositories/analysis.repository.ts
@@ -1,3 +1,7 @@
+import { Post } from "domain/Post/Post";
+import { HTTP } from "infrastructure/http/http";
+import { WP_API_POST } from "infrastructure/wordpress/config";
+
 export class AnalysisRepository {
   private static instance: AnalysisRepository;
   private constructor() {};
@@ -8,9 +12,20 @@ export class AnalysisRepository {
     }
     return AnalysisRepository.instance;
   }
-
-  createArticle = async ({category, article_args}:{category: number, article_args:{title: string}}) => {
-
+  
+  createArticle = async ({categories, wpToken,article_args}:{categories: number[],wpToken:string , article_args:{title: string, excerpt?: string}}) => {
+     const arg = {
+      ...article_args,
+      status: 'private',
+      content: '<p>Contenido de la lección aquí....</p>',
+      categories
+    }
+    const res = await HTTP.post(WP_API_POST, arg, { Authorization: `Bearer ${wpToken}` })
+    if (res.data.id) {
+      return new Post({ ...res.data, wpID: res.data.id });
+    } else {
+      return res.errCode;
+    }
   }
 }
 
