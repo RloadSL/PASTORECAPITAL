b4305e98a3f1fbbbb84b956f5d54f7d7d6eb7eac
joseacemedina <joseacemedina@gmail.com>
2022-11-10 09:55:57 +0100

    changes

--- ui/hooks/guard.permissions.hook.tsx
+++ ui/hooks/guard.permissions.hook.tsx
@@ -1,68 +1,85 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { User } from "domain/User/User"
-import { Role, Subscription } from "infrastructure/dto/users.dto"
-import { useRouter } from "next/router"
-import { useCallback, useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-import { getUserLogged } from "ui/redux/slices/authentication/authentication.selectors"
+import { User } from 'domain/User/User'
+import { Role, Subscription } from 'infrastructure/dto/users.dto'
+import { useRouter } from 'next/router'
+import { useCallback, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { getUserLogged } from 'ui/redux/slices/authentication/authentication.selectors'
 
 const system_public_module = [
-  '/', '/login', '/recover-password', '/academy', '/academy/tutorials', '/academy/courses', '/subscription', '/analysis'
+  '/',
+  '/login',
+  '/recover-password',
+  '/academy',
+  '/academy/tutorials',
+  '/academy/courses',
+  '/subscription',
+  '/analysis',
+  '/analysis/[category-slug]'
 ]
 
 const system_subscription_permission_module = {
   guest: [...system_public_module],
   basic: [...system_public_module],
-  plus:[...system_public_module, '/academy/tutorials/[tutorial-slug'],
-  premium:[...system_public_module, '/academy/tutorials/[tutorial-slug]', '/academy/courses/[course-slug]' ,'/academy/courses/[course-slug]/[lesson-slug]', '/analysis/[category-slug]'],
+  plus: [...system_public_module, '/academy/tutorials/[tutorial-slug'],
+  premium: [
+    ...system_public_module,
+    '/academy/tutorials/[tutorial-slug]',
+    '/academy/courses/[course-slug]',
+    '/academy/courses/[course-slug]/[lesson-slug]'
+    
+  ]
 }
 
-
 export const useGuardPermissions = () => {
-  const userLogged:User = useSelector(getUserLogged)
+  const userLogged: User = useSelector(getUserLogged)
   const router = useRouter()
   const [subscriptionGranted, setSubscriptionGranted] = useState(true)
 
-  const roleGranted = () => { 
-    const { role } = userLogged;  
-
+  const roleGranted = () => {
+    const { role } = userLogged
   }
 
-  
-
   const editionSetionGranted = () => {
-    if(!userLogged)  return false;
-    const { edition_section, role } = userLogged;  
-    
-    if(role.level >= 1){
-      return true;
+    if (!userLogged) return false
+    const { edition_section, role } = userLogged
+
+    if (role.level >= 1) {
+      return true
     }
 
-    return false;
+    return false
   }
 
   useEffect(() => {
-    const _subscriptionGranted = (subscription:Subscription , role:Role, route:string) => { 
-      //Total permiso para usuarios administradores  
-      if(role?.level === 2) return setSubscriptionGranted(true);
-      
-      const key_sub = subscription?.plan.key || 'guest';
+    const _subscriptionGranted = (
+      subscription: Subscription,
+      role: Role,
+      route: string
+    ) => {
+      //Total permiso para usuarios administradores
+      if (role?.level === 2) return setSubscriptionGranted(true)
+
+      const key_sub = subscription?.plan.key || 'guest'
       console.log(route)
       const authorized_sections = system_subscription_permission_module[key_sub]
       const authorized = authorized_sections.includes(route)
-      
-      setSubscriptionGranted(authorized) 
-      
+
+      setSubscriptionGranted(authorized)
     }
-    if(userLogged?.uid){
-      _subscriptionGranted(userLogged?.subscription, userLogged?.role, router.route); 
+    if (userLogged?.uid) {
+      _subscriptionGranted(
+        userLogged?.subscription,
+        userLogged?.role,
+        router.route
+      )
     }
   }, [router.route])
 
   return {
     roleGranted,
     subscriptionGranted,
-    editionGranted : editionSetionGranted(),
-    userChecked: !!userLogged 
+    editionGranted: editionSetionGranted(),
+    userChecked: !!userLogged
   }
 }
