83341a8f6c8e8e0688a871bda8e890945c80c04b
Maria <maria@rload.es>
2023-02-03 13:56:23 +0100

    askquestions layout  and details

--- pages/tax-consultant/consultants/[id]/questions.tsx
+++ pages/tax-consultant/consultants/[id]/questions.tsx
@@ -1,6 +1,3 @@
-import React from 'react'
 import ConsultantQuestions from 'ui/pages/tax-consultant/consultants/[id]/ConsultantQuestions/ConsultantQuestions';
-
-
 
 export default ConsultantQuestions;
--- ui/pages/tax-consultant/consultants/[id]/ConsultantQuestions/ConsultantQuestions.tsx
+++ ui/pages/tax-consultant/consultants/[id]/ConsultantQuestions/ConsultantQuestions.tsx
@@ -2,24 +2,26 @@ import CommentsListApp from 'components/CommentsApp'
 import { useRouter } from 'next/router'
 import ConsultantMenu from 'ui/pages/tax-consultant/components/ConsultantMenu'
 import style from './consultant-questions.module.scss'
+
 const ConsultantQuestions = () => {
   const { query } = useRouter()
 
-  
   return (
-    <div className={style.profileConversation}>
-    <ConsultantMenu />
-    <div id='askResponses'>
-      <CommentsListApp
-        infoData={{
-          mainTitle: 'Preguntas:',
-          description: 'Comenta tus dudas con el asesor.'
-        }}
-        parent={{ id: query.id as string, path: 'user_consultant' }}
-        main={true}
-      />
+    <div className={style.consultantQuestions}>
+      <ConsultantMenu />
+      <h1 className='main-title'>Pregunta a tu asesor</h1>
+
+      <div id='askResponses' className={style.consultantQuestions_container}>
+        <CommentsListApp
+          infoData={{
+            mainTitle: 'Preguntas:',
+            description: 'Comenta tus dudas con el asesor.'
+          }}
+          parent={{ id: query.id as string, path: 'user_consultant' }}
+          main={true}
+        />
+      </div>
     </div>
-  </div>
   )
 }
 
